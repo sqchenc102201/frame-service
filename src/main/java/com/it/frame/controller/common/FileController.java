305943a94e1a9f9c3e4ce3411bce5802c6e71f66
unknown <W9004028@adc.com>
2020-05-21 09:45:01 +0800

    添加模板下载接口

--- src/main/java/com/it/frame/controller/common/FileController.java
+++ src/main/java/com/it/frame/controller/common/FileController.java
@@ -8,7 +8,12 @@ import com.it.frame.vo.common.ResultVO;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,6 +21,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLEncoder;
 
@@ -55,5 +61,26 @@ public class FileController {
         return new ResultVO();
     }
 
+    @ApiOperation("下载模板文件")
+    @GetMapping(value = "/template")
+    public ResponseEntity<InputStreamResource> downloadTemple(@RequestParam String fileName) {
+        try {
+            String path = "templates" + File.separator + fileName;
+            ClassPathResource resource = new ClassPathResource(path);
+            InputStream inStream = resource.getInputStream();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Expires", "0");
+            headers.add("Pragma", "no-cache");
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            String disposition = "attachment; filename=" + URLEncoder.encode(fileName, "utf-8");
+            headers.add("Content-Disposition", disposition);
+            return ResponseEntity.ok().headers(headers)
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .body(new InputStreamResource(inStream));
+        } catch (IOException e) {
+            log.error("获取不到文件流", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 
 }
