502e4ad6b34a30abec1d9a23acbb06ef18713a56
unknown <W9004028@adc.com>
2020-05-22 17:55:48 +0800

    角色设计脚本

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,23 +1,81 @@
-CREATE TABLE `frame_test_user` (
-`id`  bigint(20) NOT NULL AUTO_INCREMENT ,
-`account`  varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL ,
-`name`  varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL ,
-`sex`  tinyint(1) NULL DEFAULT NULL ,
-`age`  int(10) NULL DEFAULT NULL ,
-PRIMARY KEY (`id`)
-)ENGING=InnoDB DEFAULT CHARSET=utf8mb4;
+create table `frame_test_user` (
+`id`  bigint(20) not null auto_increment ,
+`account`  varchar(100) character set utf8mb4 collate utf8mb4_bin null default null ,
+`name`  varchar(100) character set utf8mb4 collate utf8mb4_general_ci null default null ,
+`sex`  tinyint(1) null default null ,
+`age`  int(10) null default null ,
+primary key (`id`)
+)enging=innodb default charset=utf8mb4;
 
 
 -- 文件上传记录表
-CREATE TABLE `com_file` (
-`id`  bigint(20) NOT NULL AUTO_INCREMENT ,
-`c_uuid`  varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT 'UUID' ,
-`c_file_name`  varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '文件名' ,
-`c_file_size`  int(10) NOT NULL DEFAULT 0 COMMENT '文件大小' ,
-`c_file_path`  varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '文件路径' ,
-`c_creator_account`  varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建人工号' ,
-`c_creator_name`  varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建人姓名' ,
-`c_created`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间' ,
--- `c_updated`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间' ,
-PRIMARY KEY (`id`)
-)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8mb4_bin;
+create table `com_file` (
+`id`  bigint(20) not null auto_increment ,
+`c_uuid`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment 'uuid' ,
+`c_file_name`  varchar(500) character set utf8mb4 collate utf8mb4_bin not null comment '文件名' ,
+`c_file_size`  int(10) not null default 0 comment '文件大小' ,
+`c_file_path`  varchar(500) character set utf8mb4 collate utf8mb4_bin not null comment '文件路径' ,
+`c_creator_account`  varchar(20) character set utf8mb4 collate utf8mb4_bin null default null comment '创建人工号' ,
+`c_creator_name`  varchar(20) character set utf8mb4 collate utf8mb4_bin null default null comment '创建人姓名' ,
+`c_created`  datetime not null default current_timestamp comment '创建时间' ,
+-- `c_updated`  datetime not null default current_timestamp on update current_timestamp comment '更新时间' ,
+primary key (`id`)
+)engine=innodb auto_increment=1 default character set=utf8mb4 collate=utf8mb4_bin;
+
+
+-- 角色表
+create table `sys_role` (
+`id`  bigint(20) not null auto_increment ,
+`code`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '角色编码' ,
+`name`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '角色名称' ,
+`desc`  varchar(200) character set utf8mb4 collate utf8mb4_bin not null comment '角色描述' ,
+`status`  tinyint(1) not null default 0 comment '0：无效，1、有效' ,
+`creator`  varchar(20) character set utf8mb4 collate utf8mb4_bin null default null comment '操作人工号' ,
+`create_time`  datetime not null default current_timestamp comment '创建时间' ,
+`update_time`  datetime not null default current_timestamp on update current_timestamp comment '更新时间' ,
+primary key (`id`)
+)engine=innodb auto_increment=1 default character set=utf8mb4 collate=utf8mb4_bin;
+
+-- 角色权限表
+create table `sys_role` (
+`id`  bigint(20) not null auto_increment ,
+`role_code`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '角色编码' ,
+`permission_code`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '权限编码' ,
+`creator`  varchar(20) character set utf8mb4 collate utf8mb4_bin null default null comment '操作人工号' ,
+`create_time`  datetime not null default current_timestamp comment '创建时间' ,
+`update_time`  datetime not null default current_timestamp on update current_timestamp comment '更新时间' ,
+primary key (`id`)
+)engine=innodb auto_increment=1 default character set=utf8mb4 collate=utf8mb4_bin;
+
+-- 角色用户表
+create table `sys_role_user` (
+`id`  bigint(20) not null auto_increment ,
+`user_account`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '角色编码' ,
+`role_code`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '权限编码' ,
+`status`  tinyint(1) not null default 0 comment '0：无效，1、有效' ,
+`creator`  varchar(20) character set utf8mb4 collate utf8mb4_bin null default null comment '操作人工号' ,
+`create_time`  datetime not null default current_timestamp comment '创建时间' ,
+`update_time`  datetime not null default current_timestamp on update current_timestamp comment '更新时间' ,
+primary key (`id`)
+)engine=innodb auto_increment=1 default character set=utf8mb4 collate=utf8mb4_bin;
+
+-- 权限码表
+create table `sys_permission_code` (
+`id`  bigint(20) not null auto_increment ,
+`code`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '权限编码' ,
+`create_time`  datetime not null default current_timestamp comment '创建时间'
+primary key (`id`)
+)engine=innodb auto_increment=1 default character set=utf8mb4 collate=utf8mb4_bin;
+
+-- 基础用户表
+create table `sys_user` (
+`id`  bigint(20) not null auto_increment ,
+`account`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '用户工号' ,
+`name`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '用户姓名' ,
+`name_en`  varchar(100) character set utf8mb4 collate utf8mb4_bin not null comment '用户英文姓名' ,
+`status`  tinyint(1) not null default 0 comment '0：无效，1、有效' ,
+`create_time`  datetime not null default current_timestamp comment '创建时间' ,
+`update_time`  datetime not null default current_timestamp on update current_timestamp comment '更新时间' ,
+primary key (`id`)
+)engine=innodb auto_increment=1 default character set=utf8mb4 collate=utf8mb4_bin;
+
